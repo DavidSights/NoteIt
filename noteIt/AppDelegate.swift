f3a07456d865cdbcf35af1ee1fedf06d9584d856
DavidSights <davidseitzjr@gmail.com>
2015-12-02 14:47:38 -0600

    Basic SpeechKit setup

--- noteIt/AppDelegate.swift
+++ noteIt/AppDelegate.swift
@@ -9,10 +9,29 @@
 import UIKit
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
+
     var window: UIWindow?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
         CoreDataUtility.setupCoreData()
         return true
+    }
+
+    func setUpSpeechKit() {
+        SpeechKit.setupWithID("NMDPTRIAL_davidseitzjr_gmail_com20151116162553",
+            host: "sandbox.nmdp.nuancemobility.net",
+            port: 443,
+            useSSL: false,
+            certSummary: nil,
+            certData: nil,
+            delegate: nil)
+
+        let earconStart = SKEarcon.earconWithName("earcon_listening.wav") as! SKEarcon
+        let earconStop = SKEarcon.earconWithName("earcon_done_listening.wav") as! SKEarcon
+        let earconCancel = SKEarcon.earconWithName("earcon_cancel.wav") as! SKEarcon
+
+        SpeechKit.setEarcon(earconStart, forType: UInt(SKStartRecordingEarconType))
+        SpeechKit.setEarcon(earconStop, forType: UInt(SKStopRecordingEarconType))
+        SpeechKit.setEarcon(earconCancel, forType: UInt(SKCancelRecordingEarconType))
     }
 }
--- noteIt/ListenViewController.swift
+++ noteIt/ListenViewController.swift
@@ -10,7 +10,13 @@ import UIKit
 
 class ListenViewController: UIViewController {
 
+    let voiceSearch = SKRecognizer()
+
     @IBAction func cancelButtonPressed(sender: UIButton) {
         self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    override func viewDidLoad() {
+        SpeechKit.setupWithID("NMDPTRIAL_davidseitzjr_gmail_com20151116162553", host: "sandbox.nmdp.nuancemobility.net", port: 443, useSSL: false, certSummary: nil, certData: nil, delegate: nil)
     }
 }
